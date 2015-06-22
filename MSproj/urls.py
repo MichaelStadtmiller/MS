c4be87f8534bf760b9019c011174421de319ded1
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2015-06-21 21:47:13 -0400

    added a default homepage

--- MSproj/urls.py
+++ MSproj/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('app.urls')),
+    url(r'^$', include('home.urls')),
     url(r'^app/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
