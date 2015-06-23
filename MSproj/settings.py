645e97dd968868f9c594a435fd0a2739182ac749
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2015-06-22 23:58:15 -0400

    got the template connected. next is partysource

--- MSproj/settings.py
+++ MSproj/settings.py
@@ -101,3 +101,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
+
+# Template directory
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+#    os.path.join(os.path.dirname(__file__), 'templates')
+)
+
+
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ya home son.")
-#    return render(request, 'home/index.html)
+    return render(request, 'home/index.html')
