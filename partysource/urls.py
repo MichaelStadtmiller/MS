b0c21c667e0472b014c24e46cc86c4699c4d2838
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2015-09-18 18:56:03 -0400

    kwargs and details workinggit add * need to build to add all data

--- partysource/urls.py
+++ partysource/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-
+from partysource.views import *
 from . import views
 
 urlpatterns = [
+    # ex: /partysource/
     url(r'^$', views.index, name='index'),
+    url(r'^[0-9]+/test$', views.index, name='index'),
+    # ex: /partysource/123/details
+    url(r'^(?P<PSID>[0-9]+)/details$', BottleDetailsView.as_view(), name='bottle-details'),
 ]
--- partysource/views.py
+++ partysource/views.py
@@ -2,6 +2,7 @@
 # i think i can remove these - this was back when I could connect urls to views
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.views.generic import TemplateView, View
 from .models import Bottle
 
 # Create your views here.
@@ -16,3 +17,17 @@ def index(request):
     template = loader.get_template('partysource/index.html')
     context = RequestContext(request, {'all_bottles': all_bottles,})
     return HttpResponse(template.render(context))
+
+class BottleDetailsView(TemplateView):
+    template_name = 'partysource/bottle_details.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(BottleDetailsView, self).get_context_data(**kwargs)
+        PSID = kwargs.get('PSID') #MS ERROR? - does this need to be DB field name PSID??
+        context['bottle_details'] = self.get_bottle_details_context(PSID)
+        return context
+
+    def get_bottle_details_context(self, PSID):
+        context = {}
+        context['items'] = Bottle.objects.filter(PSID=PSID)
+        return context
