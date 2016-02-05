1996a7cd9cbeae6901129a47ee35472e96671d78
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2016-02-05 00:30:46 -0500

    added filters

--- partysource/urls.py
+++ partysource/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     # ex: /partysource/
     url(r'^$', views.index, name='index'),
-    url(r'^[0-9]+/test$', views.index, name='index'),
     # ex: /partysource/123/details
     url(r'^(?P<PSID>[0-9]+)/details$', BottleDetailsView.as_view(), name='bottle-details'),
+    # example of url path
+    #url(r'^[0-9]+/test$', views.index),
 ]
--- partysource/views.py
+++ partysource/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.views.generic import TemplateView, View
 from .models import Bottle
+import django_filters
 
 
 def index(request):
-    all_bottles = Bottle.objects.order_by('name')
+    all_bottles = BottleFilter(request.GET, queryset=Bottle.objects.all())
     template = loader.get_template('partysource/index.html')
-    context = RequestContext(request, {'all_bottles': all_bottles,},)
+    context = RequestContext(request, {'all_bottles': all_bottles, },)
     return HttpResponse(template.render(context))
 
 
@@ -34,10 +35,20 @@ class BottleDetailsView(TemplateView):
         conv_size = size
         if UOM == 'L':
             conv_size = conv_size*1000
-        context['PPU'] = round(price*(750/conv_size),5)
+        context['PPU'] = round(price*(750/conv_size), 5)
         context['imgsrc'] = imgsrc
 
         # to add the calculated values, do I add them to the context['items'] = thisBottle
         # e.g. context['items'] = thisBottle + context['PPU'] + context['imgsrc']
         return context
+
+
+class BottleFilter(django_filters.FilterSet):
+    QOH = django_filters.NumberFilter(lookup_type='lt')
+    price = django_filters.RangeFilter()
+
+    class Meta:
+        model = Bottle
+        fields = ['price', 'QOH']
+
 
